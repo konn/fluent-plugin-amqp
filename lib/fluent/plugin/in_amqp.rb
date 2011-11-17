e75581c99542a5a16f6bc8020ff456d045b4e824
Hiromi Ishii <konn.jinro@gmail.com>
2011-11-17 18:51:34 +0900

    added libs

---
+++ lib/fluent/plugin/in_amqp.rb
@@ -0,0 +1,56 @@
+module Fluent
+  class AMQPInput < Input
+    Fluent::Plugin.register_input('amqp', self)
+
+    config_param :tag, :string, :default => "hunter.amqp"
+
+    config_param :host, :string, :default => nil
+    config_param :user, :string, :default => "guest"
+    config_param :pass, :string, :default => "guest"
+    config_param :vhost, :string, :default => "/"
+    config_param :port, :integer, :default => 5672
+    config_param :queue, :string, :default => nil
+    config_param :durable, :bool, :default => false
+    config_param :exclusive, :bool, :default => false
+    config_param :auto_delete, :bool, :default => false
+    config_param :passive, :bool, :default => false
+
+    def initialize
+      require 'bunny'
+      super
+    end
+
+    
+    def configure(conf)
+      super
+      @conf = conf
+      unless @host && @queue
+        raise ConfigError, "'host' and 'queue' must be all specified."
+      end
+      @bunny = Bunny.new(:host => @host, :port => @port, :vhost => @vhost,
+                         :pass => @pass, :user => @user)
+    end
+    
+    def start
+      super
+      @thread = Thread.new(&method(:run))
+    end
+    
+    def shutdown
+      @bunny.stop
+      @thread.join
+      super
+    end
+    
+    def run
+      @bunny.start
+      q = @bunny.queue(@queue, :passive => @passive, :durable => @durable,
+                       :exclusive => @exclusive, :auto_delete => @auto_delete)
+      q.subscribe do |msg|
+        Engine.emit(@tag, Time.new.to_i, msg[:payload])
+      end
+    end # AMQPInput#run
+
+  end # class AMQPInput
+
+end # module Fluent
---
+++ lib/fluent/plugin/out_amqp.rb
@@ -0,0 +1,57 @@
+module Fluent
+  class AMQPOutput < BufferedOutput
+    Plugin.register_output("amqp", self)
+
+    config_param :host, :string, :default => nil
+    config_param :user, :string, :default => "guest"
+    config_param :pass, :string, :default => "guest"
+    config_param :vhost, :string, :default => "/"
+    config_param :port, :integer, :default => 5672
+    config_param :exchange, :string, :default => ""
+    config_param :exchange_type, :string, :default => "direct"
+    config_param :passive, :bool, :default => false
+    config_param :durable, :bool, :default => false
+    config_param :auto_delete, :bool, :default => false
+    config_param :key, :string, :default => nil
+    config_param :persistent, :bool, :default => false
+
+    def initialize
+      super
+      require "bunny"
+    end
+
+    def configure(conf)
+      super
+      @conf = conf
+      unless @host && @exchange && @key
+        raise ConfigError, "'host', 'exchange' and 'key' must be all specified."
+      end
+      @bunny = Bunny.new(:host => @host, :port => @port, :vhost => @vhost,
+                         :pass => @pass, :user => @user)
+    end
+
+    def start
+      super
+      @bunny.start
+      @exch = @bunny.exchange(@exchange, :type => @exchange_type.intern,
+                              :passive => @passive, :durable => @durable,
+                              :auto_delete => @auto_delete)
+    end
+
+    def shutdown
+      super
+      @bunny.stop
+    end
+
+    def format(tag, time, record)
+      record.to_msgpack
+    end
+
+    def write(chunk)
+      chunk.msgpack_each do |data|
+        @exch.publish(data, :key => @key, :persistent => @persistent)
+      end
+    end
+
+  end
+end
